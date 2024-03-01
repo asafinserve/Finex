ac272753b45813518efa03f90d23fdded49346f2
Abhinav Satheesh <84506819+abhinavsatheesh@users.noreply.github.com>
2024-03-01 11:41:51 +0530

    Delete gui-client directory

--- gui-client/main.py
+++
@@ -1,7 +0,0 @@
-import os
-from supabase import create_client, Client
-
-url = "https://bpgwgqutdpivvmzhxqdl.supabase.co"
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJwZ3dncXV0ZHBpdnZtemh4cWRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDUwNzY0MzUsImV4cCI6MjAyMDY1MjQzNX0.wTfP6ZIYj8PDFGs1Xqct_b4DNMzRJENlocwjikHJpfs"
-supabase: Client = create_client(url, key)
-data = supabase.table("users").insert({"mail_id":"abhinavsath7@gmail.com", "username":"Abhinav Satheesh", "user_id":1}).execute()
